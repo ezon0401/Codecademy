41fcae318b59ac6f60618f0a203824c322134570
Ji Eon Lee <ez_on@naver.com>
2024-03-18 23:35:33 +0900

    Added Toy Project

---
+++ Learn Go/Gopher It.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("JJJJJ    A    Y   Y")
+	fmt.Println("  J     A A    Y Y ")
+	fmt.Println("  J    AAAAA    Y  ")
+	fmt.Println("JJ     A   A    Y  ")
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Codecademy
-Codecademy Toy Projects
